b41dd32fd5301e6f515f44176a6fa1ca6bdefcac
khurramwaqar <khurrambaigw@gmail.com>
2022-09-01 19:48:10 +0500

    Update server.js

--- config/server.js
+++ config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  proxy: true,
+  url: env('APP_URL'), // replaces `host` and `port` properties in the development environment
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array('APP_KEYS')
   },
 });
