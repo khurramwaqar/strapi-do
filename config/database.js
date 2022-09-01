f5801ae3ea49ae2cab61e04b97062ec0c212f40b
khurram waqar <khurrambaigw@gmail.com>
2022-09-01 19:40:02 +0500

    NewUpdate

--- config/database.js
+++ config/database.js
@@ -2,17 +2,11 @@ const path = require('path');
 
 module.exports = ({ env }) => ({
   connection: {
-    client: 'postgres',
+    client: 'sqlite',
     connection: {
-      host: env('DATABASE_HOST'),
-      port: env.int('DATABASE_PORT'),
-      database: env('DATABASE_NAME'),
-      user: env('DATABASE_USERNAME'),
-      password: env('DATABASE_PASSWORD'),
-      ssl: {
-        rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false),
-      },
+      filename: env('DATABASE_FILENAME', '.tmp/data.db'),
     },
+    useNullAsDefault: true,
     debug: false,
   },
 });
