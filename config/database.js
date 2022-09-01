1328b46744c1b6283eb91d33f3a01f05210cd343
khurramwaqar <khurrambaigw@gmail.com>
2022-09-01 19:36:49 +0500

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,18 +1,18 @@
-const path = require('path');
+// path: ./config/env/production/database.js
 
-module.exports = ({ env }) => ({
-  connection: {
-    client: 'postgres',
-    connection: {
-      host: env('DATABASE_HOST'),
-      port: env.int('DATABASE_PORT'),
-      database: env('DATABASE_NAME'),
-      user: env('DATABASE_USERNAME'),
-      password: env('DATABASE_PASSWORD'),
-      ssl: {
-        rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false),
+    module.exports = ({ env }) => ({
+      connection: {
+        client: 'postgres',
+        connection: {
+          host: env('DATABASE_HOST'),
+          port: env.int('DATABASE_PORT'),
+          database: env('DATABASE_NAME'),
+          user: env('DATABASE_USERNAME'),
+          password: env('DATABASE_PASSWORD'),
+          ssl: {
+            rejectUnauthorized:env.bool('DATABASE_SSL_SELF', false),
+          },
+        },
+        debug: false,
       },
-    },
-    debug: false,
-  },
-});
+    });
